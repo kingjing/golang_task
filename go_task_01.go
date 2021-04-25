82418edda147c53aaa7a256bd162dcafb42b6490
594290 <sunguoliang@sfmail.sf-express.com>
2021-04-25 21:47:46 +0800

    第一课时作业

--- go_task_01.go
+++ go_task_01.go
@@ -1,2 +1,29 @@
-golan
+import (
+	"database/sql"
+	"fmt" 
+	"github.com/pkg/errors"
+ )
+ 
+ // DAO层
 
+ func foo() id, error {
+	db, err := sql.Open("mysql", "root:user/test") //后面格式为"user:password@/dbname"
+    defer db.Close()
+	rows, err := db.QueryRow("SELECT id from aaaaa where id=123")
+	defer rows.Close()
+	return rows.Scan(&id), errors.Wrap(sql.ErrNoRows, "foo() sql: data not found")
+ }
+ 
+ 
+ // 调用层
+ func main() {
+	_,err := foo()
+	if errors.Cause(err) == sql.ErrNoRows {
+	   fmt.Printf("data not found, %v\n", err)
+	   fmt.Printf("%+v\n", err)
+	   return
+	}
+	if err != nil {
+	   // unknown error
+	}
+ }
